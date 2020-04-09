39dde746fd749a02560fd7e8d1f7b671a7b00881
Shivani Hanji <ShivaniH@users.noreply.github.com>
2020-04-09 22:23:11 +0530

    Added static screens for retailer account and customer account

---
+++ CorEs/app/src/main/java/com/se/cores/CustomerAccount.java
@@ -0,0 +1,14 @@
+package com.example.seproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_account);
+    }
+}
---
+++ CorEs/app/src/main/java/com/se/cores/RetailerAccount.java
@@ -0,0 +1,14 @@
+package com.example.seproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetailerAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.retailer_account);
+    }
+}
