10298bea7b9dc44bc11963099f4ab808198a6d84
ShivaniH <shivanihanji@gmail.com>
2020-04-13 13:38:04 +0530

    added basic UI code

--- CorEs/app/src/main/java/com/se/cores/CustomerAccount.java
+++ CorEs/app/src/main/java/com/se/cores/CustomerAccount.java
@@ -1,8 +1,11 @@
-package com.example.seproject;
+package com.se.cores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomerAccount extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class CustomerAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_account);
+
+        Button saveButton = findViewById(R.id.saveButtonCA);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add all data to DB
+
+                // Go to app home screen
+
+                Intent home = new Intent(CustomerAccount.this, );  // go to screen 4 + 5 (home, customer logged in)
+                startActivity(home);
+            }
+        });
     }
 }
