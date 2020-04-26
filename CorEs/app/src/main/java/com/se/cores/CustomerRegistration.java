050af0721912edbff78df6ec38599e24eb077df3
ShivaniH <shivanihanji@gmail.com>
2020-04-26 15:15:44 +0530

    added code for image upload in customer reg

--- CorEs/app/src/main/java/com/se/cores/CustomerRegistration.java
+++ CorEs/app/src/main/java/com/se/cores/CustomerRegistration.java
@@ -3,19 +3,47 @@ package com.se.cores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.IOException;
+
 public class CustomerRegistration extends AppCompatActivity {
+
+    ImageButton customerImage;
+    Bitmap uploadedCustomerImage;
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_registration);
 
+        customerImage = findViewById(R.id.imageCustomerReg);
         Button saveButton = findViewById(R.id.saveButtonCR);
+
+        customerImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK);
+                pickIntent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+            }
+        });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +56,27 @@ public class CustomerRegistration extends AppCompatActivity {
                 startActivity(home);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        if (requestCode == PICK_IMAGE) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    uploadedCustomerImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    customerImage.setImageBitmap(uploadedCustomerImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
