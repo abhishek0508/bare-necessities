a5a9348c86e6f1fe75c8e6a6d631f3770a42d5cb
Abhishek <abhishek.gorisaria@alienbrains.in>
2020-04-27 22:33:24 +0530

    code refactored

--- CorEs/app/src/main/java/com/se/cores/MainActivity.java
+++ CorEs/app/src/main/java/com/se/cores/MainActivity.java
@@ -1,27 +1,26 @@
 package com.se.cores;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
+import com.google.firebase.firestore.GeoPoint;
 import java.io.Serializable;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MAIN_ACTIVITY";
     private Button button,button4,buttonShop;
     private static List<Shop> data;
-    StorageReference storageRef;
-    StorageReference imageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         data = new DatabaseAdapter().getShops();
+//        GeoPoint loc = findLocation();
+//        Log.d(TAG, "here" + String.valueOf(loc));
+//        DatabaseAdapter da = new DatabaseAdapter();
+//        List<Shop> shops = da.getShops(loc);
 
-//        storageRef = FirebaseStorage.getInstance().getReference();
-//        imageRef = storageRef.child(data.get(0).getImageUrl());
-//        final String[] URL = new String[1];
-//        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//            @Override
-//            public void onSuccess(Uri uri) {
-//                URL[0] = uri.toString();
-//            }
-//        });
-
-//        Log.d("URL Download",URL[0]);
-
-        button  = (Button)findViewById(R.id.button);
+        button  = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         buttonShop = findViewById(R.id.ShopList);
         buttonShop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,10 +69,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
     private void  openShop(){
         Intent intent = new Intent(MainActivity.this, ShopDetails.class);
         startActivity(intent);
+    }
+
+    public GeoPoint findLocation() {
+        GeoPoint loc = null;
+        Log.d("Find Location", "in find_location");
+        String location_context = Context.LOCATION_SERVICE;
+        LocationManager locationManager = (LocationManager) getSystemService(location_context);
+        List<String> providers = locationManager.getProviders(true);
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Log.d(TAG, "enabled");
+                return null;
+            }
+            locationManager.requestLocationUpdates(provider, 1000, 0,
+                    new LocationListener() {
+
+                        public void onLocationChanged(Location location) {
+                        }
+
+                        public void onProviderDisabled(String provider) {
+                        }
+
+                        public void onProviderEnabled(String provider) {
+                        }
+
+                        public void onStatusChanged(String provider, int status,
+                                                    Bundle extras) {
+                        }
+                    });
+            Location location = locationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                loc = new GeoPoint(latitude, longitude);
+                Log.d(TAG, String.valueOf(loc));
+            }
+        }
+        return loc;
     }
 }
--- CorEs/app/src/main/java/com/se/cores/ShopDetails.java
+++ CorEs/app/src/main/java/com/se/cores/ShopDetails.java
@@ -1,59 +1,47 @@
 package com.se.cores;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
-import com.google.firebase.firestore.GeoPoint;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import androidx.fragment.app.DialogFragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.widget.TextView;
-import org.w3c.dom.Text;
 import android.view.View;
 import android.widget.Button;
-import java.util.List;
-
 
 public class ShopDetails extends AppCompatActivity implements FeedbackShopStatusDialog.FeedbackShopStatusListener,
                                                               FeedbackItemAvailableDialog.FeedbackItemDialogListener,
-                                                              LoginForFeedbackDialog.LoginForFeedbackListener {
+                                                              LoginForFeedbackDialog.LoginForFeedbackListener, OnMapReadyCallback {
 
-    private static final String TAG = "shop";
-
+    private static final String TAG = "SHOP DETAILS";
+    private MapView mMapView;
     FeedBack feedBack;
     FeedbackBuilder feedbackBuilder;
     String shopID;
     private Shop shop;
-    private TextView shopName,shopOpenCloseTime;
-    
+    private TextView shopName,shopOpenCloseTime,itemAvailable,itemUnavailable;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_details);
-//        DatabaseAdapter da = new DatabaseAdapter();
-//        try {
-//            da.getShopDetails("XG4yTz2TdiH8sNWNnouP");
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-//        GeoPoint loc = findLocation();
-//        Log.d(TAG, "here" + String.valueOf(loc));
-//        List<Shop> shops = da.getShops(loc);
         DatabaseAdapter db = new DatabaseAdapter();
-
         Intent intent = getIntent();
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mMapView = findViewById(R.id.shopLocationMapView);
+        mMapView.onCreate(mapViewBundle);
+        mMapView.getMapAsync(this);
+
         shop = (Shop)intent.getSerializableExtra("SHOP");
 
         shopName = findViewById(R.id.shopNameTextView);
@@ -61,9 +49,10 @@ public class ShopDetails extends AppCompatActivity implements FeedbackShopStatus
         shopName.setText(shop.getShopName());
         String textShopOpenCloseTime = "Timings: "+shop.getOpenTime()+" to "+shop.getCloseTime();
         shopOpenCloseTime.setText(textShopOpenCloseTime);
-          
-        Button feedbackButton = findViewById(R.id.feedbackButton);
+        itemAvailable = findViewById(R.id.itemsAvailableTextView);
+        itemUnavailable = findViewById(R.id.itemsUnavailableTextView);
 
+        Button feedbackButton = findViewById(R.id.feedbackButton);
         SharedPreferences sp = getSharedPreferences("CoresPref", MODE_PRIVATE);
 
         feedbackButton.setOnClickListener(new View.OnClickListener() {
@@ -92,6 +81,55 @@ public class ShopDetails extends AppCompatActivity implements FeedbackShopStatus
     }
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        map.setMyLocationEnabled(true);
+    }
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
     public void onItemDialogPositiveClick(DialogFragment dialog) {
         feedbackBuilder.setItemAvailability(true);
     }
@@ -111,50 +149,6 @@ public class ShopDetails extends AppCompatActivity implements FeedbackShopStatus
         feedbackBuilder.setTrueStatus(false);
     }
 
-//    public GeoPoint findLocation() {
-//        GeoPoint loc = null;
-//        Log.d("Find Location", "in find_location");
-//        String location_context = Context.LOCATION_SERVICE;
-//        LocationManager locationManager = (LocationManager) getSystemService(location_context);
-//        List<String> providers = locationManager.getProviders(true);
-//        for (String provider : providers) {
-//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                // TODO: Consider calling
-//                //    ActivityCompat#requestPermissions
-//                // here to request the missing permissions, and then overriding
-//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                //                                          int[] grantResults)
-//                // to handle the case where the user grants the permission. See the documentation
-//                // for ActivityCompat#requestPermissions for more details.
-//                Log.d(TAG, "enabled");
-//                return null;
-//            }
-//            locationManager.requestLocationUpdates(provider, 1000, 0,
-//                    new LocationListener() {
-//
-//                        public void onLocationChanged(Location location) {
-//                        }
-//
-//                        public void onProviderDisabled(String provider) {
-//                        }
-//
-//                        public void onProviderEnabled(String provider) {
-//                        }
-//
-//                        public void onStatusChanged(String provider, int status,
-//                                                    Bundle extras) {
-//                        }
-//                    });
-//            Location location = locationManager.getLastKnownLocation(provider);
-//            if (location != null) {
-//                double latitude = location.getLatitude();
-//                double longitude = location.getLongitude();
-//                loc = new GeoPoint(latitude, longitude);
-//                Log.d(TAG, String.valueOf(loc));
-//            }
-//        }
-//        return loc;
-//    }
     @Override
     public void onLoginFeedbackDialogOKClick(DialogFragment dialog) {
         dialog.dismiss();
